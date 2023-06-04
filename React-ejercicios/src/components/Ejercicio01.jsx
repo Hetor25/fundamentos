99d736d80bb2a4e3fa8b48a72d218459090c41d8
Hector <hectordounis@outlook.es>
2023-06-04 09:27:18 -0600

    Cree arreglos de nombres

---
+++ React-ejercicios/src/components/Ejercicio01.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Ejercicio01 = () => {
+
+    let nombres = ["hector", "hazael","mayra","marisela","roky", "manchas","mocoso","siri","fernando","ana"] 
+
+    
+
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Ejercicio01
