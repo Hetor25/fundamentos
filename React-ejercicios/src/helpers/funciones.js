2cc7aa4fa70bb8b44c620d2618ae8cc7885e949c
Hazael Vite <14224891+HazaelVite@users.noreply.github.com>
2023-06-04 12:25:15 -0600

    funcion para generar nombres

--- React-ejercicios/src/helpers/funciones.js
+++ React-ejercicios/src/helpers/funciones.js
@@ -1,8 +1,47 @@
 export const generarNumeros = () => {
-    const numeros = [];
-    for (let i = 0; i < 5; i++) {
-        let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
-        numeros.push(numeroAleatorio);
-    }
-    return numeros;
-}
+  const numeros = [];
+  for (let i = 0; i < 5; i++) {
+    let numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+    numeros.push(numeroAleatorio);
+  }
+  return numeros;
+};
+
+export const generarNombres = () => {
+  const nombres = [
+    "Juan",
+    "María",
+    "Pedro",
+    "Ana",
+    "Luis",
+    "Laura",
+    "Carlos",
+    "Isabel",
+    "José",
+    "Sofía",
+  ];
+  const apellidos = [
+    "Gómez",
+    "Rodríguez",
+    "Martínez",
+    "López",
+    "González",
+    "Herrera",
+    "Sánchez",
+    "Díaz",
+    "Torres",
+    "Pérez",
+  ];
+
+  let nombresAleatorios = [];
+
+  for (const i = 0; i < 3; i++) {
+    const nombreAleatorio = nombres[Math.floor(Math.random() * nombres.length)];
+    const apellidoAleatorio =
+      apellidos[Math.floor(Math.random() * apellidos.length)];
+    const nombreCompleto = nombreAleatorio + " " + apellidoAleatorio;
+    nombresAleatorios.push(nombreCompleto);
+  }
+
+  return nombresAleatorios;
+};
