a44003eb0facaee518ad42adb383b51f82d976cd
Hector <hectordounis@outlook.es>
2023-06-04 11:21:08 -0600

    ejercicios de funsiones

--- React-ejercicios/src/App.jsx
+++ React-ejercicios/src/App.jsx
@@ -1,12 +1,16 @@
 import Ejercicio01 from "./components/Ejercicio01"
 import Ejercicio02 from "./components/Ejercicio02"
+import Ejercicio03 from "./components/ejecicio03"
+
 
 
 function App() {
 
   return (
     <>
-    <Ejercicio02 />
+    {/* //* //<Ejercicio02 /> *// */}
+    <Ejercicio03 />
+
     </>
   )
 }
---
+++ React-ejercicios/src/components/ejecicio03.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Sumar1(a,b) {  // funsion 
+return a+b
+}
+const Sumar2=(a,b) => {  // arroy de fleca
+    return a+b
+}
+console.log(Sumar1(1,2))
+console.log(Sumar2(2,2))
+
+
+
+const ejecicio03 = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ejecicio03
