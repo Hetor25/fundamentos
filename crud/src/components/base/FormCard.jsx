3aaa7a522e86c49c21426c0c6a8069dbfe90a1ba
Hazael Vite <14224891+HazaelVite@users.noreply.github.com>
2023-06-06 22:46:13 -0600

    Bases para proyecto de crud

---
+++ crud/src/components/base/FormCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FormInput } from './FormInput'
+import { FormButton } from './FormButton'
+
+export const FormCard = () => {
+  return (
+    <div>
+      <FormInput />
+      <FormInput />
+      <FormButton />
+    </div>
+  )
+}
