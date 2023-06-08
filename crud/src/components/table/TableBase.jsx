f172e2b3df9b3f5e50ed420b0007c2744291abcb
Hector <hectordounis@outlook.es>
2023-06-08 11:41:25 -0600

    avances

--- crud/src/components/table/TableBase.jsx
+++ crud/src/components/table/TableBase.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
+import { catalogoNombres } from '../../helpers/fakeData'
+
 
 export const TableBase = () => {
   return (
-    <div>TableBase</div>
+    <div className="container">
+      <table className="w-full">
+        <thead>
+          <tr className="p-5 m-4">
+            <td className='border border-gray-700 px-3 py-3'>Nombre</td>
+            <td className='border border-gray-700 px-3 py-3'>Apellido Paterno</td>
+            <td className='border border-gray-700 px-3 py-3'>Apellido Materno</td>
+            <td className='border border-gray-700 px-3 py-3'>Edad</td>
+            <td className='border border-gray-700 px-3 py-3'>Genero</td>
+            <td className='border border-gray-500 px-3 py-3'>Ingresar</td>
+          </tr>
+        </thead>
+      </table>
+
+ 
+      <table>
+        <thead>
+          <tr>
+            <td>{}</td>
+            <td>{}</td>
+            <td>{}</td>
+            <td>{}</td>
+            <td>{}</td>
+            <td>{}</td>
+          </tr>
+        </thead>
+      </table>
+    </div>
+
+
   )
 }
