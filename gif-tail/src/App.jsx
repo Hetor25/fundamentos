918e563a3d3edacbeb972b3a9944f2684bd33824
Hector <hectordounis@outlook.es>
2023-06-13 11:19:31 -0600

    Tailwind css

---
+++ gif-tail/src/App.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const App = () => {
+
+
+
+ // const [palabras, setPalabras] = useState(['juan', 'fernando'])
+ // const [nombreGif, setNombreGif] = useState('')
+ // console.log(e.target.value)
+ // const ParaEnviar = (e) => {
+ //   setPalabras([...palabras, "hector"])
+ //   e.preventDefault()
+ //   setNombreGif('hector')
+ // }
+
+
+  return (
+    <div className="">
+      <form className="bg-white shadow-md rounded-lg py-10 px-5s mt-5 "
+
+      >
+
+        <label className="uppercase">ingresa</label>
+        <input type="Ingresa Nombre" />
+
+
+        <button class="rounded-full mt-5 p-3 uppercase font-bold 
+                py-2
+                px-6
+                bg-sky-800
+                hover:bg-sky-500
+                text-white
+                shadow-lg"
+        >Ingresar</button>
+
+
+ </form>
+
+
+
+
+
+     
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default App
